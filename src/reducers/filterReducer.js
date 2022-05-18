57bf6cf14b0d2feb7f368f16f2b54284f1c35682
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-05-18 16:26:41 +0530

    Fixed: SmallBugs and added Readme.md file

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -2,6 +2,8 @@ export const filterReducer = (state, action) => {
   switch (action.type) {
     case "BY_CATEGORY":
       return { ...state, byCategory: [...state.byCategory, action.payload] };
+    case "REMOVE_CATEGORY": 
+      return {...state, byCategory: [...state.byCategory.filter((item) => item !== action.payload)]}
     case "CLEAR_ALL":
       return { byCategory: [] };
     default:

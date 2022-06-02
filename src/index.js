0d5fd69dfd3d13332b87d2126812f836c0e7304b
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-06-03 00:26:06 +0530

    Fix: Remove CombineContextProvider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import { CombineContextProvider } from "./context/combine-context";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
